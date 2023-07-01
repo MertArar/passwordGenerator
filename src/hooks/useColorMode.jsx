ed1aa1570d060b55aa54c815373675a4c6112583
MertArar <85193329+MertArar@users.noreply.github.com>
2023-07-02 01:21:46 +0300

    colorMode added

--- src/hooks/useColorMode.jsx
+++ src/hooks/useColorMode.jsx
@@ -2,7 +2,30 @@ import { useEffect } from "react";
 import useLocalStorage from "./useLocalStorage";
 
 const useColorMode = () => {
-  return <div>useColorMode</div>;
+  const [colorMode, setColorMode] = useLocalStorage("color-mode", "default");
+
+  useEffect(() => {
+    const className = "dark";
+    const bodyClasses = window.document.body.classList;
+
+    if (
+      colorMode === "default" &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      setColorMode("dark");
+    }
+    if (
+      colorMode === "light" &&
+      !window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      setColorMode("light");
+    }
+    colorMode === "dark"
+      ? bodyClasses.add(className)
+      : bodyClasses.remove(className);
+  }, [colorMode]);
+
+  return [colorMode, setColorMode];
 };
 
 export default useColorMode;
