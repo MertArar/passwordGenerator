441596f1e220a53ccc6aa62ba738a33f48162706
MertArar <85193329+MertArar@users.noreply.github.com>
2023-07-02 01:35:04 +0300

    app processing

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,23 @@ import useColorMode from "./hooks/useColorMode";
 
 function App() {
   const [password, setPassword] = useState("");
+  const [colorMode, setColorMode] = useColorMode();
   const [isCopied, setIsCopied] = useState(false);
   return (
     <div>
       <main>
         <section>
           <h1>Password Generator</h1>
-          <button>
-            <Icon />
+          <button
+            onClick={() =>
+              setColorMode(colorMode === "light" ? "dark" : "light")
+            }
+            className="flex items-center"
+          >
+            <Icon
+              className="p-2 text-4xl text-black border rounded-full border-greyLight dark:text-white"
+              icon={colorMode === "dark" ? "ph:moon-fill" : "ph:sun-fill"}
+            />
           </button>
         </section>
         <PasswordOutput />
