82d3df4563b9505e4e636b917afd28f599cbe302
MertArar <85193329+MertArar@users.noreply.github.com>
2023-07-02 04:06:13 +0300

    tailwind config changed

--- src/components/Generator/Generator.jsx
+++ src/components/Generator/Generator.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import { useState } from "react";
 
-const Generator = () => {
-  return <div>Generator</div>;
+import { generateCharcodesArray } from "../../utils/charcodesGenerator";
+
+import Length from "./Options/Length";
+import Parameters from "./Options/Parameters";
+import Strength from "./Strength/Strength";
+import { getRandomCharacters } from "../../utils/getRandomCharacters";
+
+const NUMBER_CHARCODES = generateCharcodesArray(48, 57);
+const SYMBOLS_CHARCODES = [
+  33, 40, 41, 45, 46, 63, 91, 93, 95, 96, 126, 59, 58, 64, 35, 36, 37, 94, 38,
+  42, 43, 61,
+];
+const UPPER_CHARCODES = generateCharcodesArray(65, 90);
+const LOWER_CHARCODES = generateCharcodesArray(97, 122);
+
+const Generator = ({ setPassword, setIsCopied }) => {
+  const [parameters, setParameters] = useState({
+    uppercase: true,
+    lowercase: true,
+    numbers: true,
+    symbols: true,
+  });
+
+  const [passwordLength, setPasswordLength] = useState(6);
+
+  const generatePassword = (parameters, length) => {
+    setIsCopied(false);
+
+    if (
+      !parameters.uppercase &&
+      !parameters.lowercase &&
+      !parameters.numbers &&
+      !parameters.symbols
+    ) {
+      return "You must select 1 option at least!";
+    }
+    let charcodes = [];
+    if (parameters.lowercase) charcodes = charcodes.concat(LOWER_CHARCODES);
+    if (parameters.uppercase) charcodes = charcodes.concat(UPPER_CHARCODES);
+    if (parameters.numbers) charcodes = charcodes.concat(NUMBER_CHARCODES);
+    if (parameters.symbols) charcodes = charcodes.concat(SYMBOLS_CHARCODES);
+    return getRandomCharacters(charcodes, length);
+  };
+
+  return (
+    <article className="flex flex-col w-full gap-4 px-6 rounded-md dark:bg-greyDark bg-greyWhite">
+      <Length />
+      <Parameters />
+      <Strength />
+      <button className="p-3 text-xl font-semibold transition-all ease-in-out rounded-md bg-primary">
+        Generate
+      </button>
+    </article>
+  );
 };
 
 export default Generator;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {},
+    colors: {
+      white: "#FCFCFC",
+      black: "#171717",
+      primary: "#FE5F55",
+      greyWhite: "#CCCCCC",
+      greyDark: "#373737",
+      success: "#007F2C",
+      transparent: "transparent",
+    },
+    fontFamily: {
+      primary: ["Inconsolata", "monospace"],
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
