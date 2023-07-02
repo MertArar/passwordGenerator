16ae44f555657f9c0b960c73756f2cabd5c2756f
MertArar <85193329+MertArar@users.noreply.github.com>
2023-07-02 04:35:43 +0300

    footer and project done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { Icon } from "@iconify/react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flex flex-col items-center py-2 mt-auto font-text">
+      <p className="text-lg">
+        Developed by <span className="font-bold">Mert Arar</span>
+      </p>
+      <div className="flex justify-center gap-2 text-4xl">
+        <a href="https://github.com/MertArar">
+          <Icon icon="mdi:github" />
+        </a>
+        <a href="https://www.linkedin.com/in/mert-arar-6788771bb/">
+          <Icon icon="mdi:linkedin" />{" "}
+        </a>
+        <a href="https://medium.com/@mertarar.ma">
+          <Icon icon="mdi:medium" />
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
